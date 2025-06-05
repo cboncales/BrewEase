0791b6dacaa1793ff881708fa0e2c59e1c80800e
cboncales <clarkboncales115@gmail.com>
2025-06-05 11:59:55 +0800

    fixed api endpoints in creating users

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+const corsOptions = {
+  origin: ["http://localhost:8000", "http://localhost:5173"], // Allow requests from both production and dev frontends
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allow necessary HTTP methods
+  credentials: true, // Allow cookies if needed
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Routes
